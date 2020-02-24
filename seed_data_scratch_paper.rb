a0628e0691567792cd826e8d23846d5aecee705b
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-02-24 07:24:37 -0800

    added seed data

---
+++ seed_data_scratch_paper.rb
@@ -0,0 +1,16 @@
+# THESE OBJECTS ARE FOR THE FRONT-END-PORTFOLIO PROJECT
+StorageArea.create!(name: "South Park", area: 12000)
+StorageArea.create!(name: "North Tarmac", area: 7000)
+StorageArea.create!(name: "Clamshell", area: 7000)
+
+Pallet.create!(name:"436L-01", priority: "first-priority", first_content: "Bottled Water", second_content: "Rice", third_content: "Sugar", weight: 198, hazmat:false, storage_area_id: 1)
+Pallet.create!(name:"436L-02", priority: "second-priority", first_content: "Canola Oil", second_content: "Craked Wheat", third_content: "WD-40", weight: 115, hazmat:true, storage_area_id: 1)
+Pallet.create!(name:"436L-03", priority: "fifth-priority", first_content: "Coils", second_content: "Sprok0ets", third_content: "Seeds", weight: 100, hazmat:false, storage_area_id: 1)
+Pallet.create!(name:"436L-04", priority: "third-priority", first_content: "Matches", second_content: "Dried Beans", third_content: "Oil", weight: 132, hazmat:true, storage_area_id: 1)
+Pallet.create!(name:"436L-05", priority: "first-priority", first_content: "Bottled Water", second_content: "Rice", third_content: "Sugar", weight: 278, hazmat:false, storage_area_id: 1)
+Pallet.create!(name:"436L-06", priority: "sixth-priority", first_content: "Canola Oil", second_content: "Craked Whe0at", third_content: "WD-40", weight: 125, hazmat:true, storage_area_id: 1)
+Pallet.create!(name:"436L-07", priority: "first-priority", first_content: "Coils", second_content: "Sprokets", third_content: "Seeds", weight: 118, hazmat:false, storage_area_id: 1)
+Pallet.create!(name:"436L-08", priority: "third-priority", first_content: "Matches", second_content: "Dried Beans", third_content: "Oil", weight: 153, hazmat:true, storage_area_id: 1)
+Pallet.create!(name:"436L-09", priority: "first-priority", first_content: "Bottled Water", second_content: "Rice", third_content: "Sugar", weight: 251, hazmat:false, storage_area_id: 1)
+Pallet.create!(name:"436L-10", priority: "second-priority", first_content: "Canola Oil", second_content: "Craked Wheat", third_content: "WD-40", weight: 243, hazmat:true, storage_area_id: 1)
+Pallet.create!(name:"436L-11", priority: "first-priority", first_content: "Coils", second_content: "Sprokets", third_content: "Seeds", weight: 118, hazmat:false, storage_area_id: 1)
