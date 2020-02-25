ca4bea8c809e8d38a9999a72ea9b7f6826646a60
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-02-25 15:31:28 -0800

    object index controller actions working

--- back-end/app/controllers/pallets_controller.rb
+++ back-end/app/controllers/pallets_controller.rb
@@ -1,2 +1,22 @@
 class PalletsController < ApplicationController
+  def index
+    pallets = Pallet.all
+    render json: pallets
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+    # pokemon = Pokemon.find_by(:id => params[:id])
+    # if pokemon.present?
+    #     Pokemon.destroy(pokemon.id)
+    #     head :no_content
+    # end
+  end
 end
--- back-end/app/controllers/storage_areas_controller.rb
+++ back-end/app/controllers/storage_areas_controller.rb
@@ -1,2 +1,22 @@
 class StorageAreasController < ApplicationController
+  def index
+    storage_area = StorageArea.all
+    render json: storage_area
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+    # pokemon = Pokemon.find_by(:id => params[:id])
+    # if pokemon.present?
+    #     Pokemon.destroy(pokemon.id)
+    #     head :no_content
+    # end
+  end
 end
